2e8d4f43bc49b94e84453b9b878edb27f5bf68fb
Esteban11p <164960060+Esteban11p@users.noreply.github.com>
2025-01-05 14:07:08 -0500

    Add files via upload

---
+++ ModalS.js
@@ -0,0 +1,59 @@
+'use strict'
+
+/* Habre el modal para REGISTRARTE */
+
+const user = document.getElementById('user');
+const theModal = document.getElementById('modal');
+
+const close = document.getElementById('delete');
+const close2 = document.getElementById('delete2');
+
+// modo register
+
+const Mregister = document.getElementById('modal_Register');
+
+user.addEventListener('click', function() {
+  Mregister.style.display = 'block'
+  theModal.style.display = 'block';
+});
+
+close.addEventListener('click', function() {
+  theModal.style.display = 'none';
+});
+
+// modo Crear Cuenta
+
+const crear = document.getElementById('Bcrear');
+const Mcrear_cuenta = document.getElementById('Mcrear_cuenta');
+
+crear.addEventListener('click', function() {
+  Mcrear_cuenta.style.display = 'block';
+  Mregister.style.display = 'none'
+  theModal.style.display = 'block';
+});
+
+close2.addEventListener('click', function() {
+  theModal.style.display = 'none';
+});
+
+
+
+
+/* Habre el modal para VENDER tu ganado */
+
+const vender = document.getElementById('Vender');
+const theModalV = document.getElementById('modalV-GAN');
+
+
+// modo register
+vender.addEventListener('click', function() {
+ 
+  theModalV.style.display = 'block';
+});
+
+
+
+
+
+
+
